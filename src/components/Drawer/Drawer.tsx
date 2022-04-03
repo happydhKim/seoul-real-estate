298566c3c4e8dac2b6303613631f386bcb40ed38
happydhKim <happytime870616@gmail.com>
2022-04-03 23:00:24 +0900

    [Component] Drawer 코드 정리 - #5

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -1,43 +1,43 @@
-import * as React from 'react';
+import { memo } from 'react';
 import { css } from '@emotion/react';
+
+import type { FC, ReactNode, CSSProperties } from 'react';
 
 type DrawerProps = {
   open: boolean;
   onClose?: () => void;
-  children?: React.ReactNode;
+  children?: ReactNode;
 };
 
-const Drawer: React.FC<DrawerProps> = (props) => {
+const Drawer: FC<DrawerProps> = (props) => {
   const { open, onClose, children } = props;
 
-  const overlayStyles: React.CSSProperties = {
+  const overlayStyles: CSSProperties = {
     display: open ? 'block' : 'none',
   };
 
-  const transitionStyles: React.CSSProperties = {
+  const transitionStyles: CSSProperties = {
     transform: open ? 'translate3d(0%, 0, 0)' : 'translate3d(-100%, 0, 0)',
     transitionDuration: '500ms',
   };
 
   return (
-    <div id={'drawer'} className="drawer" css={css``}>
-      {open && (
-        <div
-          style={overlayStyles}
-          onClick={onClose}
-          css={css`
-            position: fixed;
-            width: 100%;
-            height: 100%;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            background-color: rgba(0, 0, 0, 0.5);
-            z-index: 2;
-          `}
-        />
-      )}
+    <>
+      <div
+        style={overlayStyles}
+        onClick={onClose}
+        css={css`
+          position: fixed;
+          width: 100%;
+          height: 100%;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          background-color: rgba(0, 0, 0, 0.5);
+          z-index: 2;
+        `}
+      />
       <nav
         style={transitionStyles}
         css={css`
@@ -52,8 +52,8 @@ const Drawer: React.FC<DrawerProps> = (props) => {
       >
         {children}
       </nav>
-    </div>
+    </>
   );
 };
 
-export default Drawer;
+export default memo(Drawer);
