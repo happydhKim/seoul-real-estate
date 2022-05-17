8dd4a56c7082570b61988470dc39d08fd2713a03
happydhKim <happytime870616@gmail.com>
2022-05-17 22:46:29 +0900

    [Component] TextArea 컴포넌트 개발 - #50

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,4 @@ export { default as Footer } from './Footer';
 export { default as Header } from './Header';
 export { default as Input } from './Input';
 export { default as SelectBox } from './SelectBox';
+export { default as TextArea } from './TextArea';
