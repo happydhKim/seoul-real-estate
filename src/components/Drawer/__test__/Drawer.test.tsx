1c2839e607736852cc64acf52d66406c8b45567a
happydhKim <happytime870616@gmail.com>
2022-06-29 22:45:29 +0900

    [Test] Drawer router test 추가 - #67

--- src/components/Drawer/__test__/Drawer.test.tsx
+++ src/components/Drawer/__test__/Drawer.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import {createMemoryHistory} from 'history';
 import {Router} from 'react-router-dom';
 import Drawer from '../Drawer';
@@ -30,5 +30,22 @@ describe('Drawer', () => {
     texts.forEach((text) => {
       expect(container).toHaveTextContent(text);
     });
+  });
+
+  it('링크를 클릭하면 해당 path로 이동이 된다.', async () => {
+    const {getAllByText} = renderDrawer();
+
+    const firstMenu = getAllByText('첫 번째 링크');
+    const secondMenu = getAllByText('두 번째 링크');
+    const thirdMenu = getAllByText('세 번째 링크');
+    
+    await fireEvent.click(firstMenu[0]);
+    await expect(history.location.pathname).toBe('/first');
+
+    await fireEvent.click(secondMenu[0]);
+    await expect(history.location.pathname).toBe('/second');
+
+    await fireEvent.click(thirdMenu[0]);
+    await expect(history.location.pathname).toBe('/third');
   });
 });
